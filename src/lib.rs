da14ffa2a5675c754735bf1c814b60ffb067fe8b
Tobin C. Harding <me@tobin.cc>
2023-12-05 11:39:42 +1100

    Add doc_auto_cfg
    So that the HTML rendering of our docs marks feature gated code enable
    `doc_auto_cfg` when we build docs for docsrs.

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,7 @@
 #![cfg_attr(all(not(test), not(feature = "std")), no_std)]
 // Experimental features we need.
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 // Coding conventions
 #![warn(missing_docs)]
 
