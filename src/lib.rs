3f6ad85183548e2a7626f56bf19c58ea2bc8e156
Tobin C. Harding <me@tobin.cc>
2025-03-04 09:28:53 +1100

    Introduce empty crate
    Introduce an empty crate and trimmed down CI.
    
    CI runs on merge into `1.x` branch is based on the idea that development
    is done on `master` and we only merge code into `1.x` that has already
    been tested there. CI here is basically sanity checks. We run the
    `run_task` script with
    
    - stable
    - msrv
    - docs

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! Hex encoding and decoding.
+//!
+//! General purpose hex encoding/decoding library with a conservative MSRV and dependency policy.
+//!
+//! ## Stabilization strategy
+//!
+//! In an effort to release stable 1.0 crates that are forward compatible we are striving
+//! relentlessly to release the bare minimum amount of code.
+
+#![cfg_attr(all(not(test), not(feature = "std")), no_std)]
+// Experimental features we need.
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+// Coding conventions
+#![warn(missing_docs)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
