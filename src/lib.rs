308d5138ad4d0bd5e519b705fafd0f99d6f0f887
Tobin C. Harding <me@tobin.cc>
2025-03-04 09:36:11 +1100

    WIP: Introduce decoding logic
    WIP because this should come from `master` but we are waiting on #166 to
    merge.
    
    `master` is at tip: TOOD
    
    Introduce decoding logic from `master`, this means:
    
    Public API consisting of:
    
    - The crate level decoding functions
    - The associated error types
    
    Private code:
    
    - Code in private modules `parse` and `iter` as needed.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@
 //! ## Stabalization strategy
 //!
 //! In an effort to release stable 1.0 crates that are forward compatible we are striving
-//! relentlessly to release the bare minimum amount of code.
+//! relentlessly to release the bare minimum amount of code. This 1.0 version currently holds
+//! only two hex parsing functions and the associated error types.
 
 #![cfg_attr(all(not(test), not(feature = "std")), no_std)]
 // Experimental features we need.
@@ -25,4 +26,31 @@ pub mod _export {
     pub mod _core {
         pub use core::*;
     }
+}
+
+mod error;
+mod iter;
+mod parse;
+
+#[cfg(all(feature = "alloc", not(feature = "std")))]
+use alloc::vec::Vec;
+
+use crate::parse::FromHex as _;
+
+#[rustfmt::skip]                // Keep public re-exports separate.
+#[doc(inline)]
+pub use self::{
+    error::{
+        HexToArrayError, HexToBytesError, InvalidCharError, InvalidLengthError,
+        OddLengthStringError, ToArrayError, ToBytesError,
+    },
+};
+
+/// Decodes a hex string into a vector of bytes.
+#[cfg(feature = "alloc")]
+pub fn decode_vec(s: &str) -> Result<Vec<u8>, HexToBytesError> { Vec::from_hex(s) }
+
+/// Decodes a hex string into an array of bytes.
+pub fn decode_array<const N: usize>(s: &str) -> Result<[u8; N], HexToArrayError> {
+    <[u8; N]>::from_hex(s)
 }
