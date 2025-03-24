0eb281c6980405cafeacaaf49c5760434756ea77
Tobin C. Harding <me@tobin.cc>
2025-03-25 09:25:51 +1100

    Improve crate level docs
    Mention the new `decode_` functions in the crate level docs.

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,16 @@
 //! // In your manifest use the `package` key to improve import ergonomics.
 //! // hex = { package = "hex-conservative", version = "*" }
 //! # use hex_conservative as hex; // No need for this if using `package` as above.
-//! use hex::prelude::*;
+//! use hex::prelude::*;  // Imports `FromHex` and `Display`.
 //!
 //! // Decode an arbitrary length hex string into a vector.
 //! let v = Vec::from_hex("deadbeef").expect("valid hex digits");
 //! // Or a known length hex string into a fixed size array.
 //! let a = <[u8; 4]>::from_hex("deadbeef").expect("valid length and valid hex digits");
+//!
+//! // If you don't want to import the `FromHex` trait use inherent functions instead.
+//! let v = hex::decode_vec("deadbeef").expect("even string length and valid hex digits");
+//! let a = hex::decode_array::<[u8; 4]>("deadbeef").expect("valid length and valid hex digits");
 //!
 //! // We support `LowerHex` and `UpperHex` out of the box for `[u8]` slices.
 //! println!("An array as lower hex: {:x}", a.as_hex());
