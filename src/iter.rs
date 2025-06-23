7143ac171c74c61ab5502dd241398a9b88355fe1
Tobin C. Harding <me@tobin.cc>
2025-06-23 13:08:00 +1000

    Remove the HexToBytesIter from the public API
    Face palm! - Thanks goodness for the RC cycle. No clue how this slipped
    by us.

--- src/iter.rs
+++ src/iter.rs
@@ -29,7 +29,8 @@ impl<'a> HexToBytesIter<HexDigitsIter<'a>> {
     ///
     /// If the input string is of odd length.
     #[inline]
-    pub fn new(s: &'a str) -> Result<Self, OddLengthStringError> {
+    #[allow(dead_code)] // Remove this when making HexToBytesIter public.
+    pub(crate) fn new(s: &'a str) -> Result<Self, OddLengthStringError> {
         if s.len() % 2 != 0 {
             Err(OddLengthStringError { len: s.len() })
         } else {
