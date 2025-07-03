d54622d19fb923b99e95ba5a37b9098d4cf53a30
Martin Habovstiak <martin.habovstiak@gmail.com>
2025-07-03 11:22:25 +0200

    Add API test for the functions
    The most important functions - `decode_to_array` and `decode_to_vec`
    didn't have the API tests, so this commit adds it.

--- tests/api.rs
+++ tests/api.rs
@@ -14,11 +14,6 @@ use hex_conservative::{
     InvalidLengthError, OddLengthStringError,
 };
 
-// Some arbitrary data to use.
-const HEX: &str = "deadbeef";
-const BYTES: [u8; 8] = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
-const CAP: usize = 16; // BYTES.len() * 2
-
 /// A struct that includes all public error types.
 // These derives are the policy of `hex-conservative` not Rust API guidelines.
 #[derive(Debug, Clone, PartialEq, Eq)] // All public types implement Debug (C-DEBUG).
@@ -39,4 +34,51 @@ fn all_types_implement_send_sync() {
     // Error types should implement the Send and Sync traits (C-GOOD-ERR).
     assert_send::<Errors>();
     assert_sync::<Errors>();
+}
+
+const VALID: &str = "deadbeef";
+const VALID_UPPER: &str = "DEADBEEF";
+const VALID_MIXED: &str = "dEaDBeEf";
+const TOO_SHORT: &str = "deadbee";
+const TOO_LONG: &str = "deadcaffe";
+const INVALID_CHAR: &str = "deadreef";
+const EXPECTED: [u8; 4] = [0xde, 0xad, 0xbe, 0xef];
+
+#[test]
+fn decode_to_array() {
+    assert_eq!(hex_conservative::decode_to_array::<4>(VALID).unwrap(), EXPECTED);
+    assert_eq!(hex_conservative::decode_to_array::<4>(VALID_UPPER).unwrap(), EXPECTED);
+    assert_eq!(hex_conservative::decode_to_array::<4>(VALID_MIXED).unwrap(), EXPECTED);
+    match hex_conservative::decode_to_array::<4>(TOO_SHORT).unwrap_err() {
+        DecodeFixedLengthBytesError::InvalidLength(error) => assert_eq!(error.invalid_length(), 7),
+        DecodeFixedLengthBytesError::InvalidChar(_) => panic!("unexpected error"),
+    }
+    match hex_conservative::decode_to_array::<4>(TOO_LONG).unwrap_err() {
+        DecodeFixedLengthBytesError::InvalidLength(error) => assert_eq!(error.invalid_length(), 9),
+        DecodeFixedLengthBytesError::InvalidChar(_) => panic!("unexpected error"),
+    }
+    match hex_conservative::decode_to_array::<4>(INVALID_CHAR).unwrap_err() {
+        DecodeFixedLengthBytesError::InvalidLength(_) => panic!("unexpected error"),
+        DecodeFixedLengthBytesError::InvalidChar(error) => assert_eq!(error.pos(), 4),
+    }
+}
+
+#[cfg(feature = "alloc")]
+#[test]
+fn decode_to_vec() {
+    assert_eq!(hex_conservative::decode_to_vec(VALID).unwrap(), EXPECTED);
+    assert_eq!(hex_conservative::decode_to_vec(VALID_UPPER).unwrap(), EXPECTED);
+    assert_eq!(hex_conservative::decode_to_vec(VALID_MIXED).unwrap(), EXPECTED);
+    match hex_conservative::decode_to_vec(TOO_SHORT).unwrap_err() {
+        DecodeVariableLengthBytesError::OddLengthString(error) => assert_eq!(error.length(), 7),
+        DecodeVariableLengthBytesError::InvalidChar(_) => panic!("unexpected error"),
+    }
+    match hex_conservative::decode_to_vec(TOO_LONG).unwrap_err() {
+        DecodeVariableLengthBytesError::OddLengthString(error) => assert_eq!(error.length(), 9),
+        DecodeVariableLengthBytesError::InvalidChar(_) => panic!("unexpected error"),
+    }
+    match hex_conservative::decode_to_vec(INVALID_CHAR).unwrap_err() {
+        DecodeVariableLengthBytesError::OddLengthString(_) => panic!("unexpected error"),
+        DecodeVariableLengthBytesError::InvalidChar(error) => assert_eq!(error.pos(), 4),
+    }
 }
